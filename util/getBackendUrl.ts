a1df724fc3ce66c581dc7138edb766b550dc1838
haakoaho <hakon.hoff@outlook.com>
2024-07-05 22:32:32 +0200

    change url for fetching the backend

--- util/getBackendUrl.ts
+++ util/getBackendUrl.ts
@@ -4,7 +4,7 @@ let backendUrl : string | null;
 
 export async function getBackendUrl() {
   if(backendUrl) return backendUrl;
-  const response = await fetch("https://haakoaho.github.io/speak-fun/deployments.json");
+  const response = await fetch("https://haakoaho.github.io/speak-fun/deployments/meeting-planner.json");
   const data : Deployments = await response.json();
   backendUrl = data.backendUrl;
   return data.backendUrl;
