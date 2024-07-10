5f56ac1fc33d055dcf35e89818e98a4c9b4d1e5b
haakoaho <hakon.hoff@outlook.com>
2024-07-10 08:48:03 +0200

    Update getBackendUrl.ts

--- util/getBackendUrl.ts
+++ util/getBackendUrl.ts
@@ -3,7 +3,7 @@ import { Deployment } from "../types";
 let backendUrl : string | null;
 
 export function getBackendUrl() {
-  return "http://mobile-speak.asuscomm.com:8081"
+  return "https://mobile-speak.asuscomm.com:8081"
 }
 
 export function invalidateBackendUrl(){
